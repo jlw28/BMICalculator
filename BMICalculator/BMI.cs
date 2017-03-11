a36980a8f44648d96580b11eb117a3b37d8e626d
jlw28 <jlwimsatt@gmail.com>
2017-03-11 09:15:52 -0500

    Add project files.

---
+++ BMICalculator/BMI.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+namespace BMICalculator
+{
+    /// <summary>
+    /// Class used to calculate BMI for both metric and imperial
+    /// </summary>
+    class BMI
+    {
+        //formula for meters: weight / (height * height)  ****height in meters; weight in kilograms
+        //formula in imperial: (weight / (height * height)) * 703     *****height in inches; weight in pounds
+
+        private double weight;
+        private double height;
+        private double result;
+        private string unit_type;
+
+        public BMI()
+        {
+            unit_type = "metric";
+        }
+
+        public void SetHeight(double _height)
+        {
+            height = _height;
+        }
+
+        public void SetWeight(double _weight)
+        {
+            weight = _weight;
+        }
+
+        public void SetUnitType(string unit)
+        {
+            unit_type = unit;
+        }
+        public string GetUnitType()
+        {
+            return unit_type;
+        }
+       
+
+        public void Calculate()
+        {
+            result = 0;
+            if(unit_type == "metric")
+            {
+                
+                result = weight / (Math.Pow(height, 2));
+            }
+            else
+            {
+                result = (weight / (Math.Pow(height, 2)));
+                result = result * 703;
+            }
+        }
+
+        public string Results()
+        {
+            if(result >= 18.5 && result <= 25)
+            {
+                return "Your BMI is " + Math.Round(result, 1) + "\nYou are within the ideal weight range.";
+            }
+            else
+            {
+                if(result < 18.5)
+                {
+                    return "Your BMI is " + Math.Round(result, 1) + "\nYou are underweight. You should see your doctor.";
+                }
+                else
+                {
+                    return "Your BMI is " + Math.Round(result, 1) + "\nYou are overweight. You should see your doctor.";
+                }
+            }
+        }
+    }
+}
---
+++ BMICalculator/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Windows;
+
+
+namespace BMICalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        BMI calc = new BMI();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Changes height inputs and input labels
+        private void toggle_IsCheckedChanged(object sender, EventArgs e)
+        {
+            if(toggle.IsChecked == true)
+            {
+                
+                tb_height.Visibility = Visibility.Hidden;
+                lbl_mH.Visibility = Visibility.Hidden;
+
+                tb_ft.Visibility = Visibility.Visible;
+                tb_in.Visibility = Visibility.Visible;
+                lbl_ft.Visibility = Visibility.Visible;
+                lbl_in.Visibility = Visibility.Visible;
+                lbl_mW.Content = "lbs";
+                calc.SetUnitType("imperial");
+                
+            }
+            else
+            {
+                tb_ft.Visibility = Visibility.Hidden;
+                tb_in.Visibility = Visibility.Hidden;
+                lbl_ft.Visibility = Visibility.Hidden;
+                lbl_in.Visibility = Visibility.Hidden;
+
+                tb_height.Visibility = Visibility.Visible;
+                lbl_mH.Visibility = Visibility.Visible;
+                lbl_mW.Content = "kgs";
+                calc.SetUnitType("metric");
+            }
+  
+        }
+
+        //Calculates bmi based on user input
+        private void btn_calc_Click(object sender, RoutedEventArgs e)
+        {
+            string height = "";
+            string inch = "";
+            string weight = tb_weight.Text;
+            int inches = 0;
+
+            if (calc.GetUnitType() == "metric")
+            {
+                height = tb_height.Text;
+               
+            }else
+            {
+                height = tb_ft.Text;
+                inch = tb_in.Text;
+               
+            }
+
+            string result = "";
+            double h = 0;
+
+            if(double.TryParse(height, out h))
+            {
+                double w = 0;
+                if(double.TryParse(weight, out w))
+                {
+                    if(calc.GetUnitType() == "imperial")
+                    {
+
+                        if (Int32.TryParse(inch, out inches))
+                        {
+                            string total = FeetToInches(height, inches);
+                            calc.SetHeight(Convert.ToDouble(total));
+
+                            calc.SetWeight(w);
+                            calc.Calculate();
+                            result = calc.Results();
+                            tb_results.Text = result;
+                        }
+                        else
+                        {
+                            result = "Inches must be a number. Please try again.";
+                            tb_results.Text = result;
+                        }
+
+                    }
+                    else
+                    {
+                        calc.SetHeight(h);
+
+                        calc.SetWeight(w);
+                        calc.Calculate();
+                        result = calc.Results();
+                        tb_results.Text = result;
+                    }
+                    
+                }
+                else
+                {
+                    result = "Height and Weight must be a number. Please try again.";
+                    tb_results.Text = result;
+                }
+
+
+            }
+            else
+            {
+                result = "Height and Weight must be a number. Please try again.";
+                tb_results.Text = result;
+            }
+
+
+        }
+
+
+        //Converts feet to inches and totals height
+        private string FeetToInches(string _ft, int _in)
+        {
+            int ft = Convert.ToInt32(_ft);
+            int total = (ft * 12) + _in;
+
+            return total.ToString();
+        }
+    }
+}
